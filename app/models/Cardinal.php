84e10c2bf43115012a3a0d8a9633625939cb3611
César D. Rodas <crodas@php.net>
2013-11-22 20:03:29 -0300

    Get audio from cardinal :-)

---
+++ app/models/Cardinal.php
@@ -0,0 +1,37 @@
+<?php
+
+
+/**
+ *  @Persist
+ */
+class Cardinal extends Noticia
+{
+    /** @Bool */
+    public $is_audio = true;
+
+    public static function is($url)
+    {
+        return preg_match('/cardinal\.com/', $url);
+    }
+
+    public function crawl()
+    {
+        if ($this->crawled) return;
+
+        echo "Crawling {$this->url}\n";
+
+        $content = $this->wget($this->url);
+        $dom = new \DomDocument;
+        @$dom->loadHTML($content);
+        $xpath = new \DOMXPath($dom);
+
+        $title = $this->text($xpath->query('//*[@id="noticias-list"]//div[@class="description"]/a'));
+        $mp3   = $xpath->query('//*[@id="noticias-list"]//div[@class="links"]/a[1]')->item(0)->getAttribute('href');
+        $html  = $xpath->query('//*[@id="embed_code"]')->item(0)->getAttribute('value');
+        $this->titulo = $title;
+        $this->texto  = $title;
+
+        var_dump($title, $mp3, $html);exit;
+    }
+
+}
--- app/models/Noticia.php
+++ app/models/Noticia.php
@@ -52,7 +52,28 @@ abstract class Noticia
         foreach ($object as $node) {
             $text[] = trim($node->textContent);
         }
-        return implode("\n", $text);
+
+        return trim(implode("\n", $text));
+    }
+
+    protected function wget($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_USERAGENT =>  'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 GTB5',
+        ]);
+        $html = curl_exec($ch);
+        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+        curl_close($ch);
+
+        if (is_callable('tidy_repair_string')) {
+            // thanks cardinal for given me a hard time serving broken html
+            $html = tidy_repair_string($html, array('wrap' => 0), 'utf8');
+        }
+
+        return ForceUTF8\Encoding::toUTF8($html);
     }
 
     public static function getOrCreate($url)
@@ -60,7 +81,8 @@ abstract class Noticia
         $db  = Service::get("db");
         $col = $db->getCollection(__CLASS__)->findOne(['url' => $url]);
         if (empty($col)) {
-            $col = new self;
+            $class = self::getType($url);
+            $col = new $class;
             $col->url = $url;
         }
 
@@ -69,7 +91,7 @@ abstract class Noticia
 
     public static function getType($url)
     {
-        foreach (array('Abc') as $type) 
+        foreach (array('Abc', 'Cardinal') as $type) 
         {
             if ($type::is($url)) {
                 return $type;
@@ -77,6 +99,12 @@ abstract class Noticia
         }
         return false;
     }
+
+    protected static function is_useful_internal($url)
+    {
+        return true;
+    }
+
 
     public static function is_useful($url) 
     {
