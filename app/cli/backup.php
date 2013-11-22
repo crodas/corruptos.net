7740d2e52701190784b443d779f9d24344e615ff
César D. Rodas <crodas@php.net>
2013-11-22 18:22:07 -0300

    restore backup from internet

--- app/cli/backup.php
+++ app/cli/backup.php
@@ -1,9 +1,23 @@
 <?php
 
 /** 
+ *  @Cli("db:restore") 
+ */
+function restore($input, $output)
+{
+    $target = __DIR__ . '/../../';
+    $uniqid = uniqid(True) . ".tar.xz";
+    `wget -O {$uniqid} http://corruptos.net/db.tar.xz`;
+    `tar xfv $uniqid`;
+    `mongorestore --drop dump`;
+    `rm -rf dump`;
+    unlink($uniqid);
+}
+
+/** 
  *  @Cli("db:backup") 
  */
-function index($input, $output)
+function backup($input, $output)
 {
     $target = __DIR__ . '/../../public_html';
     `cd /tmp; mongodump -d corruptos_net; tar cfv db.tar dump; xz db.tar ; mv db.tar.xz $target`;
