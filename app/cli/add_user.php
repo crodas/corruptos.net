46a6bce8860f229131f5fdc71be8893389e59e36
César D. Rodas <crodas@php.net>
2013-11-22 00:53:24 -0300

    more debug

--- app/cli/add_user.php
+++ app/cli/add_user.php
@@ -15,6 +15,7 @@ function update($input, $output)
 {
     $conn = Service::get('db');
     foreach ($conn->getCollection('corruptos')->Find() as $corrupto) {
+        echo "Actualizando {$corrupto->nombre}\n";
         $corrupto->update();
         $conn->save($corrupto);
     }
