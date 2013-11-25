c44d0b1a4f77c7f3fc5b3f09ad56cfaff217ae06
César D. Rodas <crodas@php.net>
2013-11-24 21:50:25 -0300

    remove portada from toplinks

--- app/libs/middleware/common.php
+++ app/libs/middleware/common.php
@@ -20,7 +20,6 @@ function inject_is_mobile($req, $unused, $args)
 function inject_menu($req, $unused, $args)
 {
     $args['menu'] = array_merge([
-            '/' => ['Portada', false],
             '/ver_los23' => ['Los 23', false],
             '/ver_senador' => ['Senadores', false],
             '/ver_diputado' => ['Diputados', false],
