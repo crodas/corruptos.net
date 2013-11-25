e5e245cf17987b8b2f2efe1f9d8c055b96e8c36b
César D. Rodas <crodas@php.net>
2013-11-25 05:28:48 -0300

    we dont care bout sports

--- app/models/Paraguay.php
+++ app/models/Paraguay.php
@@ -8,6 +8,11 @@ class Paraguay extends Noticia
         return preg_match('/paraguay\.com\//', $url);
     }
 
+    protected static function is_useful_internal($url)
+    {
+        return !preg_match('/deportes/i', $url);
+    }
+
     public function crawl()
     {
         if ($this->crawled) return;
