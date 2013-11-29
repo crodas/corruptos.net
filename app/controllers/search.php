5cb387c5a154ea312a5cf847c07a14b7f08aafd9
César D. Rodas <crodas@php.net>
2013-11-29 01:44:18 -0300

    fulltext search

---
+++ app/controllers/search.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *  @Route "/busqueda/{text}"
+ *  @Route "/busqueda/{text}/{page}"
+ *  @Route "/busqueda"
+ *  @View result.tpl
+ */
+function get_corruptos_medios($req)
+{
+
+    $index = Service::get('esearch');
+    $page  = intval($req->get('page'));
+    $text  = urldecode($req->get('text') ?: '');
+
+    if (!empty($_GET['q'])) {
+        header("Location: /busqueda/" . urlencode($_GET['q']));
+        exit;
+    }
+    if (empty($text)) {
+        header("Location: /");
+        exit;
+    }
+
+    $q = new \Elastica\Query\QueryString();
+    $q->setDefaultOperator('AND');
+    $q->setQuery($text);
+
+    $query = new \Elastica\Query();
+    //$query->setFilter($tfilter);
+    $query->setQuery($q);
+    $query->setFrom(($page)*20);    // Where to start?
+    $query->setLimit(20);   // How many?
+
+    $results = $index->search($query);
+    $form = new crodas\Form\Form;
+    $form->populate(['q' => $text]);
+
+    $base = "/busquedas/" . urlencode($text);
+    $has_next = $results->getTotalHits() > $page * 20;
+
+    return compact('results', 'text', 'form', 'page', 'base', 'has_next');
+}
