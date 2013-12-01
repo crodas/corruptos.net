c32dba8ee13d460018139af3bd06faf30b44559e
César D. Rodas <crodas@php.net>
2013-12-01 04:38:39 -0300

    adding log provider

---
+++ app/services/log.php
@@ -0,0 +1,21 @@
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/**
+ *  @Service("logger", {
+ *      name: {default: 'botame-log'},
+ *      dir: { default: '/tmp', type: dir },
+ *  }, {shared: true})
+ */
+function log_service($config, $context)
+{
+    // create a log channel
+    $name  = !empty($context['name']) ? $context['name'] : $config['name'];
+    $fname = $name . date("Y-m-d") . ".log"; 
+    $log = new Logger($name);
+    $log->pushHandler(new StreamHandler($config['dir'] . '/' . $fname, Logger::WARNING));
+
+    return $log;
+}
