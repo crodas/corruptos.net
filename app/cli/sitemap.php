4707e0ee97f13b27c5c0f07d09d1ecadef8ce32e
César D. Rodas <crodas@php.net>
2013-12-02 04:12:47 -0300

    minor issue

--- app/cli/sitemap.php
+++ app/cli/sitemap.php
@@ -19,7 +19,7 @@ function sitemap()
 
     $generator->limit(1000);
     $generator->addMap($db->getCollection('noticias')->find()->sort(['$natural' => -1])->limit(20000), function($corrupto) {
-        if (empty($corruptos->corruptos)) {
+        if (empty($corrupto->corruptos)) {
             return null;
         }
         return "/noticia/" . $corrupto->uri;
